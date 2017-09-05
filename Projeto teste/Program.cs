8ef42df67a6c7c43d27bb7e484d8c8c94356868b
Diego Rodrigues <diegorod1983@gmail.com>
2017-09-05 15:08:52 -0300

    adicionado uma pergunta

--- Projeto teste/Program.cs
+++ Projeto teste/Program.cs
@@ -12,6 +12,7 @@ namespace Projeto_teste
         {
             Console.WriteLine("Ola mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como vc estava?");
 
             Console.ReadLine();
 
